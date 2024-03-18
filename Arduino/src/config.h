883ba169dad7d6937f5f5975b5ad87e9e6955129
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-18 17:06:35 +0100

    sets pin to work

--- Arduino/src/config.h
+++ Arduino/src/config.h
@@ -1,7 +1,22 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
+/***
+ * @brief analog pins setting
+*/
 constexpr int POT_PIN = A1;
+constexpr int SDA_PIN = A4;
+constexpr int SCL_PIN = A5;
+
+/***
+ * @brief digital pins setting
+*/
+constexpr int GATE_PIN = 5;
+constexpr int BUTTON_PIN = 2;
+
+
+
+
 
 
 #endif
