e17c61d4ac4c952197ad08f8b4bbccada4be4a8f
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-21 17:39:12 +0100

    implements gest method for viwe sys status

--- Arduino/src/model/Gateway.h
+++ Arduino/src/model/Gateway.h
@@ -7,55 +7,63 @@
 #include "devices/ServoMotorImpl.h"
 #include "devices/Lcd.h"
 
-
 class Gateway
 {
-
-public:
-    Gateway();
-
-    /***
-     * @brief do digital read on button pin 
-    */
-    bool buttonRead();
-    /***
-     * @brief change mode from auto to manual and vice versa
-    */
-    void ChangeMode();
-    /**
-     * @brief create all device object
-     * and initialize to auto mode to detect 
-     * degree of gate opening/closing 
-     ***/
-    void init();
-    /**
-     * @brief set gate degree to open/close
-     ***/
-    void setGateDegree(int degree);
-    
-    /**
-     * @brief get the current degree of the gate
-     ***/
-    int getGateDegree();
-    
-
-    /**
-     * @brief get value of potentiometer
-     ***/
-    int getPotentioMeterValue();
-
-
-    ~Gateway();
 private:
     int gateDegree;
     bool automode;
     Lcd *lcd;
     ServoMotorImpl *servoMotor;
     ButtonImpl *button;
+    SysTatus state;
+
+public:
+    SysTatus getState() const
+    {
+        return state;
+    }
+
+    void setState(SysTatus newState)
+    {
+        state = newState;
+    }
+    Gateway();
+
+    /***
+     * @brief do digital read on button pin
+     */
+    bool buttonRead();
+    /***
+     * @brief change mode from auto to manual and vice versa
+     */
+    void changeMode();
+    /**
+     * @brief create all device object
+     * and initialize to auto mode to detect
+     * degree of gate opening/closing
+     ***/
+    void init();
+    /**
+     * @brief set gate degree to open/close
+     ***/
+    void setGateDegree(int degree);
+
+    /**
+     * @brief get the current degree of the gate
+     ***/
+    int getGateDegree();
+
+    /**
+     * @brief get value of potentiometer
+     ***/
+    int getPotentioMeterValue();
+
+    /**
+     * @brief print the current state system on I2C LCD
+     ***/
+    void pritnState();
+
+    ~Gateway();
 };
-
-
-
-
 
 #endif // GATEWAY_H
