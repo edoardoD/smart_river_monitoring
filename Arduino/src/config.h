18856df404b5c91d2ab1e76094f1e64e2ca44162
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-21 17:35:00 +0100

    define sysStem status class and tostring method

--- Arduino/src/config.h
+++ Arduino/src/config.h
@@ -1,21 +1,44 @@
+
 #ifndef __CONFIG__
 #define __CONFIG__
-
+#include <arduino.h>
+#include <string.h>
 /***
  * @brief analog pins setting
-*/
-constexpr int POT_PIN = A1;
-constexpr int SDA_PIN = A4;
-constexpr int SCL_PIN = A5;
+ */
+#define POT_PIN = A1;
+#define SDA_PIN = A4;
+#define SCL_PIN = A5;
 
 /***
  * @brief digital pins setting
-*/
+ */
 constexpr int GATE_PIN = 5;
 constexpr int BUTTON_PIN = 2;
 
-
-
+/***
+ * @brief enum class for system status
+ */
+enum class SysTatus
+{
+    AUTO,
+    MANUAL
+};
+/***
+ * @brief enum class to string of system status
+*/
+String enum_toString(SysTatus status)
+{
+    switch (status)
+    {
+        case SysTatus::AUTO:
+            return "AUTO";
+        case SysTatus::MANUAL:
+            return "MANUAL";
+        default:
+            return "UNKNOWN";
+    }
+}
 
 
 
