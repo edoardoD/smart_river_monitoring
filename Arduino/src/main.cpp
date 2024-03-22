4c409a7a0572c5ba880a6b55804228813ce6777f
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-22 16:55:34 +0100

    setted all startup init

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 #include "kernel/Scheduler.h"
 #include "Kernel/Task.h"
+#include "model/Gateway.h"
+#include "task/ModeTask.h"
+#include "task/WorkerTask.h"
+
 
 #define DEBUG 
 Scheduler sched = Scheduler(50);
+Gateway gtw = Gateway();
 
 void setup() {
-  
+  gtw.init();
+  ModeTask modeTask = ModeTask(&gtw, 50);
+  sched.addTask(&modeTask);
+  WorkerTask workerTask = WorkerTask(&gtw, 100);
+  sched.addTask(&workerTask);
+  Serial.begin(9600);
+  Serial.println("Start");
+
 }
 
 void loop() {
