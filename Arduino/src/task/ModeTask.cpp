22c7cc644c388b1424ed5b946680977cbedf2b98
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-23 16:04:49 +0100

    correct logic for button pulldown

--- Arduino/src/task/ModeTask.cpp
+++ Arduino/src/task/ModeTask.cpp
@@ -1,10 +1,11 @@
 #include "ModeTask.h"
 #include "Arduino.h"
-
+#include "model/logger.h"
 ModeTask::ModeTask(Gateway *gateway, int period) : TaskWithState(period)
 {
     gtw = gateway;
     setState(BtnState::RELEASED);
+    
 }
 
 void ModeTask::tick()
@@ -12,15 +13,15 @@ void ModeTask::tick()
     switch (this->getState())
     {
     case BtnState::RELEASED:
-        Serial.println("RELEASED \n");
-        if (!gtw->buttonRead())
+        logger("Released");
+        if (gtw->buttonRead())
         {
             setState(BtnState::PRESSING);
         }
         break;
     case BtnState::PRESSING:
-        Serial.println("PRESSING \n");
-        if (gtw->buttonRead())
+        logger("Pressing");
+        if (!gtw->buttonRead())
         {
             gtw->changeMode();   
             setState(BtnState::RELEASED);
