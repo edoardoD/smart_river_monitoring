ad13298388d245f7d52bd3b33bc84975306c98ab
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-03-26 16:56:52 +0100

    delate useless include

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,14 +1,14 @@
-#include <Arduino.h>
+  #include <Arduino.h>
 #include "kernel/Scheduler.h"
 #include "Kernel/Task.h"
 #include "model/Gateway.h"
 #include "task/ModeTask.h"
 #include "task/WorkerTask.h"
-
-
+  
+  
 #define DEBUG 
 Scheduler sched = Scheduler(50);
-Gateway gtw = Gateway();
+Gateway gtw = Gateway();  
 
 void setup() {
   gtw.init();
