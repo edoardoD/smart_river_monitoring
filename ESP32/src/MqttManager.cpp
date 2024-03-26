b5a24a4316e699d06666d5ec44dabd934dbf2bdf
Michele Olivieri <micheleolivieri02@gmail.com>
2024-03-26 18:10:20 +0100

    improving esp main

---
+++ ESP32/src/MqttManager.cpp
@@ -0,0 +1,26 @@
+#include "MqttManager.h"
+
+MQTTManager::MQTTManager(const char* mqtt_server, const char* topic)
+    : _mqtt_server(mqtt_server),
+      _topic(topic), 
+      _client(_espClient) {
+}
+
+void MQTTManager::setup() {
+    _client.setServer(_mqtt_server, 1883);
+}
+
+void MQTTManager::reconnect() {
+    while ( !_client.connected() ) {
+        String clientId = String("esiot-2122-client") + String(random(0xfffff), HEX);
+        if ( _client.connect(clientId.c_str()) ) {
+            _client.subscribe(_topic);
+        } else {
+            delay(500);
+        }
+    }
+}
+
+void MQTTManager::publish(const char* msg) {
+    _client.publish(_topic, msg);
+}
