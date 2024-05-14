575110fca898ae500e88c6ac69adf54e774c6b18
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2024-05-14 13:38:10 +0200

    update time shedule

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -11,14 +11,12 @@ Scheduler sched = Scheduler(100);
 Gateway gtw = Gateway();  
 
 void setup() {
+  MsgService.init();
   gtw.init();
-  ModeTask modeTask = ModeTask(&gtw, 20);
+  ModeTask modeTask = ModeTask(&gtw, 100);
   sched.addTask(&modeTask);
-  WorkerTask workerTask = WorkerTask(&gtw, 50);
+  WorkerTask workerTask = WorkerTask(&gtw, 150);
   sched.addTask(&workerTask);
-  Serial.begin(9600);
-  Serial.println("Start");
-
 }
 
 void loop() {
